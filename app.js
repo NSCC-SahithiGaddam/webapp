0bda153c53bb5617f698706720e7d2cae11e25a9
Sahithi Gaddam <gaddam.sahi@northeastern.edu>
2023-10-04 19:52:46 -0400

    added testcase

--- app.js
+++ app.js
@@ -62,7 +62,23 @@ router.patch('/*', isAuth, async (req, res) => {
 })
 
 app.get('/healthz', async (req, res)=>{
-  await authenticate(req, res)
+  res.setHeader('cache-control', 'no-cache');
+    if (Object.keys(req.body).length > 0) {
+        return res.status(400).send();
+    }
+    if (Object.keys(req.query).length > 0){
+        return res.status(400).send();
+    }
+    try{
+        await sequelize.authenticate()
+        console.log("Successfully connected to MySQL")
+        res.status(200)
+    }
+    catch(err){
+        console.log("Unable to connect to MySQL", err)
+        res.status(503)
+    }
+    res.end()
 });
 
 app.all('/healthz', (req, res) => {
