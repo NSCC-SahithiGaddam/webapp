2fca8097f03e607ea890185e43b91d0c62019b9f
Sahithi Gaddam <113363655+sahithir27@users.noreply.github.com>
2023-10-04 20:30:20 -0400

    Feature/api (#1)
    * implemented db creation, insertion
    
    * implemented basic authentication
    
    * added assignment endpoints
    
    * added testcase
    
    * added github workflow

---
+++ src/authorization.js
@@ -0,0 +1,34 @@
+const readfromdb = require('./database/read')
+const bcrypt = require('bcrypt');
+const readAuthHeaders = (req,res) => {
+    const auth = req.headers.authorization;
+    if (!auth) {
+      res.status(401).send('Unauthorized');
+      return;
+    }
+    const [email, password] = Buffer.from(auth.split(' ')[1], 'base64').toString('utf8').split(':');
+    return [email, password]
+}
+
+const authorizationCheck = async (req, res, next) => {
+    // const auth = req.headers.authorization;
+    // if (!auth) {
+    //   res.status(401).send('Unauthorized');
+    //   return;
+    // }
+    //const [email, password] = Buffer.from(auth.split(' ')[1], 'base64').toString('utf8').split(':');
+    const [email, password] = readAuthHeaders(req,res)
+    const user = await readfromdb.findUser(email);
+    if (!user) {
+        res.status(401).send('Unauthorized');
+        return; 
+      }
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      res.status(401).send('Unauthorized');
+      return;
+    }
+    next();
+}
+
+module.exports = {readAuthHeaders, authorizationCheck}
---
+++ src/database/insert.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+const sequelize = require('../database/bootstrap')
+const user = require('../models/User');
+
+const bcrypt = require("bcrypt")
+const saltRounds = 10
+
+const csvFilePath = '/opt/Users.csv';
+const finduser = async (email) => {
+    const userdata = await user.findOne({where: {email: email}})
+    if(userdata){
+        return true
+    }
+    else return false
+}
+
+const encryptPassword = async (password) => {
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    return hashedPassword
+}
+
+const insert_into_db = async () => {
+    fs.createReadStream(csvFilePath)
+    .pipe(csv())
+    .on('data', async (data) => {
+      const isUserExists = await finduser(data.email)
+      const encryptedPassword = await encryptPassword(data.password)
+      if(!isUserExists){
+        user.create({
+            first_name: data.first_name,
+            last_name: data.last_name,
+            email: data.email,
+            password: encryptedPassword
+          })
+            .then(() => {
+              console.log('Record inserted');
+            })
+            .catch((error) => {
+              console.error('Error inserting record:', error);
+            });
+      }
+      else{
+        console.error('User already exists')
+      }
+      
+    })
+    .on('end', () => {
+      console.log('CSV data import complete.');
+    });
+}
+  module.exports = insert_into_db
