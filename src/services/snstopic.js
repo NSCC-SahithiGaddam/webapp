6a41570967c87ef831c6c1b637ecb2696a3ad9a2
Sahithi Gaddam <gaddam.sahi@northeastern.edu>
2023-11-29 02:46:36 -0500

    added region for sdk

--- src/services/snstopic.js
+++ src/services/snstopic.js
@@ -2,8 +2,9 @@ const AWS = require('aws-sdk');
 const logger = require('../../logger')
 require('dotenv').config();
 const topicARN = process.env.SNS_TOPIC_ARN
-
+const aws_region = process.env.AWS_REGION
 const sns = new AWS.SNS();
+AWS.config.update({ region: aws_region });
 const publishMessage = (submission_url, email) => {
     const messageObject = {
         submissionUrl: submission_url,
